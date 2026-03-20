b096b5cbc1df900f54b79bcb6b4ac5e362761770
Tomjati123 <ppricha02@gmail.com>
2026-03-20 10:30:04 +0700

    แserver.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
-
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server started"));
